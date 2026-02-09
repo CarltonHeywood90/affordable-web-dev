c20633ae32a098a072808a912ee3456501f910de
carlton <cjwood1@gmail.com>
2026-02-09 13:33:45 -0700

    removed privacy policy link from footer

--- js/footer.js
+++ js/footer.js
@@ -3,6 +3,5 @@ const footer = document.getElementById('footer');
 footer.innerHTML = `
     <footer>
         <p>&copy; ${new Date().getFullYear()} Affordable Web Dev. All rights reserved.</p>
-        <p><a href="contact.html">Contact Us</a> | <a href="privacy.html">Privacy Policy</a></p>
     </footer>
 `;
